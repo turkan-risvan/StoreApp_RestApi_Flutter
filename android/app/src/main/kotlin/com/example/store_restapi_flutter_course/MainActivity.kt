483b468670290a8dd39e44dd9071f7fe22e3c9b8
Türkan Rişvan <turkanrisvan887@gmail.com>
2023-09-06 09:10:12 +0300

    Store RestApi Flutter

---
+++ android/app/src/main/kotlin/com/example/store_restapi_flutter_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.store_restapi_flutter_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widget/sale_widget.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class SaleWidget extends StatelessWidget {
+  const SaleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        gradient: const LinearGradient(
+            colors: [
+              Color.fromARGB(255, 61, 17, 132),
+              Color.fromARGB(255, 3, 83, 118),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9689CE),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    // mainAxisSize: MainAxisSize.max,
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Get the special discount",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 18,
+                      ),
+                      Flexible(
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text(
+                              "50 %\nOFF",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                // fontSize: 300,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.network(  
+                "https://i.ibb.co/vwB46Yq/shoes.png",
+              // width: double.infinity,
+              fit: BoxFit.contain,
+
+                ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
